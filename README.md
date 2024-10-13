b3d73f9cc95f2fba5f8b3c22031aeac0480a6eb6
Chiron H <chironathome@gmail.com>
2024-10-13 20:00:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# CPU Temp Display
+A shell script to display CPU temperature in a customizable way.
+
+## Installation
+Clone this repository and run `makepkg -si` to install.
+
+### Detailed Install:
+First: `git clone https://github.com/Chiron8/TempTell` this copies this repo's packages into the current folder you are in
+
+Then: `cd TempTell` to change directory to the copied files
+
+Finally: `makepkg -si` makes this package executable
+
+## Usage
+
+```bash
+cpu-temp [-c COLOR] [-f FORMAT]
---
+++ cpu-temp.sh
@@ -0,0 +1,16 @@
+# Maintainer: Chiron8 https://github.com/Chiron8/Chiron8
+pkgname=temptell
+pkgver=1.0
+pkgrel=1
+pkgdesc="A script to display CPU temperature with customizable options"
+arch=('any')
+url="https://github.com/Chiron8/TempTell"
+license=('MIT')
+depends=('lm_sensors')
+source=("$pkgname-$pkgver.tar.gz")
+sha256sums=('SKIP')
+
+package() {
+    install -Dm755 "$srcdir/cpu-temp.sh" "$pkgdir/usr/bin/cpu-temp"
+    install -Dm644 "$srcdir/LICENSE" "$pkgdir/usr/share/licenses/$pkgname/LICENSE"
+}
